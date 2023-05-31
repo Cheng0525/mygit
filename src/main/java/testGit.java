b06e3840565af85f6fdc48c818588cd006209049
cheng <cheng@163.com>
2023-05-31 22:42:21 +0800

    00123

--- src/main/java/testGit.java
+++ src/main/java/testGit.java
@@ -1,6 +1,6 @@
 public class testGit {
     public static void main(String[] args){
-        System.out.println("001");
+        System.out.println("002");
     }
     //System.out.println("001");
 }
